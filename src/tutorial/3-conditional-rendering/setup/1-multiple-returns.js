d71c83ae7a9d6a1969c3a591ba9294dc0b5aa19e
shalini-polapally <polapallyshalini1@gmail.com>
2022-10-19 20:19:29 +0530

    multiple returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,40 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isloading,setIsLoading] = useState(true)
+  const [isError,setIsError] = useState(false);
+  const [user,setUser] = useState('default user');
+
+  useEffect(()=>{
+    fetch(url).then((response)=>{
+      if(response.status>=200 && response.status<=299){
+        return response.json()
+      }
+      else{
+        setIsLoading(false);
+        setIsError(true)
+        throw new Error(response.statusText)
+      }
+    })
+    .then((user)=>{
+      const {login}=user;
+      setUser(login);
+      setIsLoading(false);
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  },[])
+
+  if(isloading){
+    return<><h1>Loading....</h1></>
+  }
+  if(isError){
+    return<>
+    <h1>Error....</h1>
+    </>
+  }
+    return <><h1>{user}</h1></>
 };
 
 export default MultipleReturns;
