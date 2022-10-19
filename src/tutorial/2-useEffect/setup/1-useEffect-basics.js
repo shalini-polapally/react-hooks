63615da3451dc5141c5cf1877c0a44181a4b0632
shalini-polapally <polapallyshalini1@gmail.com>
2022-10-19 18:37:54 +0530

    useEffect basics and second parameter

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,25 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value,setValue] = useState(0);
+  const [val,setVal] = useState(0);
+    useEffect(()=>{
+      console.log('call useEffect');
+      if(value>1){
+        document.title = `New Messages(${value})`;
+      }
+    },[value])
+  return (
+    <>
+    <h1>{value}</h1>
+    <button className="btn" onClick={()=> setValue(value+1)}>
+      Click Me!
+    </button>
+    <button className="btn" onClick={()=> setVal(val+1)}>
+      Click Me Not
+    </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
