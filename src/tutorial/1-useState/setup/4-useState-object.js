9ec3ac2a30095f8e5dfa5f476289328915bc2a38
shalini-polapally <polapallyshalini1@gmail.com>
2022-10-19 13:48:49 +0530

    useState Objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person,setPerson] = useState({name:"shalini",age:26,message:"random message"});
+  const [name,setName] = useState('shalini');
+  const [age,setAge] = useState(26);
+  const [message,setMessage] = useState('Random message');
+
+  const changeMessage = () =>{
+    setMessage("Message changed")
+  }
+  return (<>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{message}</h3>
+  <button className="btn" onClick={changeMessage}>Change message</button>
+  </>);
 };
 
 export default UseStateObject;
